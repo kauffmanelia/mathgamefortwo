efa5545c725626f14ad4b072fd4b8ba3f006592d
kardar233 <kauffmanelia@gmail.com>
2014-10-07 12:55:36 -0700

    Control flow for game start and replay built. WarGames flavour added.

--- mathgamefortwo/main.c
+++ mathgamefortwo/main.c
@@ -7,7 +7,30 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
 
-int main(void) {
+int main(void){
+    srand(time(NULL));
+    printf("GREETINGS PROFESSOR FALKEN\n");
+    printf("SHALL WE PLAY A GAME?\n");
+    printf("I KNOW YOU WOULD PREFER A NICE GAME OF CHESS, OR POSSIBLY GLOBAL THERMONUCLEAR WAR, BUT CURRENTLY I AM ONLY PROGRAMMED FOR A FACILE GAME OF MATH FOR TWO PLAYERS.\nIF YOU ARE STILL INTERESTED, ENTER YES. IF NOT, ENTER NO.");
+    char input[10];
+    char startstring[] = "yes\n";
+    char exitstring[] = "no\n";
+    while(true){
+        fgets(input, 10, stdin);
+        if(strcmp(input, exitstring) == 0) break; // if 'no' is entered, exit
+        else if(strcmp(input, startstring) != 0){
+            printf("THAT IS NOT A VALID RESPONSE. PLEASE ENTER A VALID RESPONSE. ");
+            continue;
+        }
+        
+        
+        
+        printf("THIS GAME HAS ENDED. SHALL WE PLAY ANOTHER GAME? ");
+    }
     return 0;
 }
