28fcc1acd516b4b23eb6b495c65b5584e3ea870e
kardar233 <kauffmanelia@gmail.com>
2014-10-07 12:38:11 -0700

    Initial Commit

---
+++ mathgamefortwo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mathgamefortwo
+//
+//  Created by Elia Kauffman on 2014-10-07.
+//  Copyright (c) 2014 Elia Kauffman. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
